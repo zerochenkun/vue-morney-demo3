8358b97e934af5d0ffa08f5bc709121d583e4900
chenuvi <chenuvi@outlook.com>
2020-09-15 23:01:47 +0800

    style:money.vue css初步

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>money.vue
+  <div class="nav-wrapper">
+    <p class="content">money.vue</p>
     <Nav />
   </div>
 </template>
@@ -11,5 +12,13 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border:1px solid red;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    flex-grow: 1;
+  }
 </style>
