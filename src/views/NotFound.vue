bcf8ac42aabe39b7b764760aa8076dbc704d4076
chenuvi <chenuvi@outlook.com>
2020-09-15 21:29:15 +0800

    feat:添加NotFound组件 404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>当前页面错误，请输入正确网址</div>
+    <div>
+      <router-link to="/money">返回首页</router-link>
+
+<!--      <a href="/">返回首页 by 'a' </a>-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
