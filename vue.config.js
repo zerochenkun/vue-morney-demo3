a6aca32b0263a9acada14237e842d522c32a6879
chenuvi <chenuvi@outlook.com>
2020-09-19 19:31:52 +0800

    fix:解决 svg fill 问题

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,8 @@ module.exports = {
       .include.add(dir).end()  // *只针对dir 也就是 icons 目录下的 .svg文件*
       //{extract:false} 不要解析为文件
       .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract:false}).end()
-
+      .use('svgo-loader').loader('svgo-loader')
+      .tap(options => ({...options,plugins:[{removeAttrs:{attrs:'fill'}}]})).end
     //配置 svg-sprite 插件
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite:true}])
