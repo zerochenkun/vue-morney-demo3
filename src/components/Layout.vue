6edf986fed0dbfde43df37b549442000fc0a519a
chenuvi <chenuvi@outlook.com>
2020-09-30 23:53:25 +0800

    style:classPrefix 添加与修复

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="nav-wrapper">
-    <p class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`">
       <slot></slot>
-    </p>
+    </div>
     <Nav />
   </div>
 </template>
 
 <script lang="ts">
   export default {
+    props:['classPrefix'],
     name: 'Layout'
   };
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper{
+  .layout-wrapper{
     display: flex;
     flex-direction: column;
     height: 100vh;
   }
   .content{
+    overflow: auto;
     flex-grow: 1;
   }
 </style>
