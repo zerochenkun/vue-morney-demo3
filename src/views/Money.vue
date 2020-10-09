7a2e48d5f22871ff9a21ab9925647d3de23faa97
zerochenkun <chenuvi@outlook.com>
2020-10-09 22:20:24 +0800

    refactor:model.ts 获取数据

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,28 +15,21 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
-const model = require('@/model.js').model
-console.log(model);
+import {model} from '@/model.ts'
 
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-  createDate?: Date;
-}
+
 @Component({
   components: {Notes, Tags, Types, NumberPad},
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '理财', '娱乐', '教育'];
-  record: Record = {
+  record: RecordItem = {
     tags : [],
     notes :  '',
     type :  '-',//'-'表示支出 '+'表示收入
     amount : 0
   };
-  recordList: Record[] = model.fetch()
+  recordList = model.fetch()
 
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -51,14 +44,13 @@ export default class Money extends Vue {
   }
 
   saveRecord(){
-    const deepCloneRecord = JSON.parse(JSON.stringify(this.record))
-    deepCloneRecord.createDate = new Date()
-    this.recordList.push(deepCloneRecord)
+    const record2 = model.deepClone(this.record)
+    this.recordList.push(record2)
   }
 
   @Watch('recordList')
   onRecordListChange(){
-    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+    model.save(this.recordList)
   }
 }
 </script>
