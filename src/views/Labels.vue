77650f4817c434f282f60f2c881a98ba20a48a9e
zerochenkun <chenuvi@outlook.com>
2020-10-09 23:10:14 +0800

    feat:Label.vue' html and style

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,18 +1,68 @@
 <template>
   <Layout>
-    Labels.vue
+    <ul class="tags" >
+      <li>
+        <span>衣</span>
+        <Icon name="right"></Icon>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"></Icon>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"></Icon>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"></Icon>
+      </li>
+    </ul>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
   </Layout>
 
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Labels',
-  };
+import Vue from 'vue';
+
+export default class Labels extends Vue {
+}
 </script>
 
 <style lang="scss" scoped>
-  p {
-    color:red;
+  .tags{
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
+    > li {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      min-height: 44px;
+      border-bottom: 1px solid #e6e6e6;
+      > svg{
+        width: 18px;
+        height: 18px;
+        color:#666;
+        margin-right: 16px;
+      }
+    }
+  }
+
+  .createTag {
+    background:  #767676;
+    color: white;
+    border:none;
+    border-radius:4px;
+    height: 40px;
+    padding:0 16px;
+    &-wrapper {
+      text-align: center;
+      padding: 16px;
+      margin-top: 28px;
+    }
   }
 </style>
