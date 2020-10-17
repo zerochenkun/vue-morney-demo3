32e865efd1100d65835c6a7b64ef941aac5117df
zerochenkun <chenuvi@outlook.com>
2020-10-17 15:16:26 +0800

    feat:将 tag 的 string 改为 {id, name} & new router:editLabel

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,19 @@
+<template>
+  <layout>
+    编辑标签
+  </layout>
+</template>
+
+<script>
+import Vue from "vue";
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class EditLabels extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,8 @@
 <template>
   <Layout>
     <ul class="tags" >
-      <li v-for="tag in tags" :key="tag">
-        <span>{{tag}}</span>
+      <li v-for="tag in tags" :key="tag.id">
+        <span>{{tag.name}}</span>
         <Icon name="right"></Icon>
       </li>
     </ul>
